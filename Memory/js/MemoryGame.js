5bb47380a354e07a9ec555083abc9a8118596782
MonaKabelka <mona.kabelka.mk@gmail.com>
2021-01-28 21:13:11 +0100

    buttons

--- Memory/js/MemoryGame.js
+++ Memory/js/MemoryGame.js
@@ -1,13 +1,134 @@
 var MemoryGame;
 (function (MemoryGame) {
-    var EasyCards = [
-        // Kartenpaar 1
+    // Wrapper für die Buttons als Variablen
+    var btnWrapper = document.getElementById("buttons");
+    // Buttons erstellen
+    var btnEasy = document.createElement("button");
+    btnEasy.innerHTML = "Easy"; //wieso wird nur text von letztem button angezeigt?
+    btnWrapper.appendChild(btnEasy);
+    var btnMedium = document.createElement("button");
+    btnMedium.innerHTML = "Medium";
+    btnWrapper.appendChild(btnMedium);
+    var btnHard = document.createElement("button");
+    btnHard.innerHTML = "Hard";
+    btnWrapper.appendChild(btnHard);
+    // geht das mit den buttons auch kürzer?
+    btnEasy.addEventListener("click", function () {
+        console.log("Difficulty: Easy");
+        btnEasy.classList.add("hidden");
+        btnHard.classList.add("hidden");
+        btnMedium.classList.add("hidden");
+    });
+    btnMedium.addEventListener("click", function () {
+        console.log("Difficulty: Medium");
+        btnEasy.classList.add("hidden");
+        btnHard.classList.add("hidden");
+        btnMedium.classList.add("hidden");
+    });
+    btnHard.addEventListener("click", function () {
+        console.log("Difficulty: Hard");
+        btnEasy.classList.add("hidden");
+        btnHard.classList.add("hidden");
+        btnMedium.classList.add("hidden");
+    });
+    var allCards = [
         {
             color: "#CCFFCA",
-            pictogram: "fab fa-pagelines",
+            pictogram: "fas fa-bolt",
             scentence: "",
             compare: 1
+        },
+        {
+            color: "",
+            pictogram: "fas fa-wind",
+            scentence: "",
+            compare: 2
+        },
+        {
+            color: "#84FE7F",
+            pictogram: "fas fa-smog",
+            scentence: "",
+            compare: 3
+        },
+        {
+            color: "#84FE7F",
+            pictogram: "fas fa-cloud",
+            scentence: "",
+            compare: 4
+        },
+        {
+            color: "#8BCA89",
+            pictogram: "fas fa-sun",
+            scentence: "",
+            compare: 5
+        },
+        {
+            color: "#8BCA89",
+            pictogram: "fas fa-umbrella",
+            scentence: "",
+            compare: 6
+        },
+        {
+            color: "#619F5F",
+            pictogram: "fas fa-rainbow",
+            scentence: "",
+            compare: 7
+        },
+        {
+            color: "#619F5F",
+            pictogram: "fas fa-cloud-sun-rain",
+            scentence: "",
+            compare: 8
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-sun",
+            scentence: "",
+            compare: 9
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-showers-heavy",
+            scentence: "",
+            compare: 10
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-rain",
+            scentence: "",
+            compare: 11
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-moon-rain",
+            scentence: "",
+            compare: 12
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-meatball",
+            scentence: "",
+            compare: 13
+        },
+        {
+            color: "",
+            pictogram: "fas fa-temperature-low",
+            scentence: "",
+            compare: 14
+        },
+        {
+            color: "",
+            pictogram: "far fa-snowflake",
+            scentence: "",
+            compare: 15
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-moon",
+            scentence: "",
+            compare: 16
         }
     ];
+    var cardDOMElement;
 })(MemoryGame || (MemoryGame = {}));
 //# sourceMappingURL=MemoryGame.js.map
--- Memory/js/MemoryGame.ts
+++ Memory/js/MemoryGame.ts
@@ -1,67 +1,150 @@
 namespace MemoryGame {
+    // Wrapper für die Buttons als Variablen
+    const btnWrapper: HTMLElement = document.getElementById("buttons");
 
-interface CardsInterface {
-    color: string;
-    pictogram: string;
-    scentence: string;
-    compare: number;
-}
+    // Buttons erstellen
+    let btnEasy: HTMLButtonElement = document.createElement("button");
+    btnEasy.innerHTML = "Easy"; //wieso wird nur text von letztem button angezeigt?
+    btnWrapper.appendChild(btnEasy);
 
-let EasyCards: CardsInterface [] = [ //Muss man jedes Objekt zweimal erstellen?? Kann man das nicht mit while schleifen machen?
-    // Kartenpaar 1
-    {
-        color: "#CCFFCA",
-        pictogram: "fab fa-pagelines",
-        scentence: "", //geht das?? weil man nur die anderen sachen braucht
-        compare: 1
-    },
-    {
-        color: "#CCFFCA",
-        pictogram: "fab fa-pagelines",
-        scentence: "", 
-        compare: 1
-    },
-    // Kartenpaar 2
-    {
-        color: "#84FE7F",
-        pictogram: "fas fa-tree",
-        scentence: "", 
-        compare: 2
-    },
-    {
-        color: "#84FE7F",
-        pictogram: "fas fa-tree",
-        scentence: "", 
-        compare: 2
-    },
-    // Kartenpaar 3
-    {
-        color: "#8BCA89",
-        pictogram: "fas fa-wine-bottle",
-        scentence: "", 
-        compare: 3
-    },
-    {
-        color: "#8BCA89",
-        pictogram: "fas fa-wine-bottle",
-        scentence: "", 
-        compare: 3
-    },
-    // Kartenpaar 4
-    {
-        color: "#619F5F",
-        pictogram: "fas fa-bacterium",
-        scentence: "", 
-        compare: 4
-    },
-    {
-        color: "#619F5F",
-        pictogram: "fas fa-bacterium",
-        scentence: "", 
-        compare: 4
+    let btnMedium: HTMLButtonElement = document.createElement("button");
+    btnMedium.innerHTML = "Medium";
+    btnWrapper.appendChild(btnMedium);
+
+    let btnHard: HTMLButtonElement = document.createElement("button");
+    btnHard.innerHTML = "Hard";
+    btnWrapper.appendChild(btnHard);
+
+    // geht das mit den buttons auch kürzer?
+
+    btnEasy.addEventListener("click", function (): void {
+        console.log("Difficulty: Easy");
+        btnEasy.classList.add("hidden");
+        btnHard.classList.add("hidden");
+        btnMedium.classList.add("hidden");
+    });
+
+    btnMedium.addEventListener("click", function (): void {
+        console.log("Difficulty: Medium");
+        btnEasy.classList.add("hidden");
+        btnHard.classList.add("hidden");
+        btnMedium.classList.add("hidden");
+    });
+
+    btnHard.addEventListener("click", function (): void {
+        console.log("Difficulty: Hard");
+        btnEasy.classList.add("hidden");
+        btnHard.classList.add("hidden");
+        btnMedium.classList.add("hidden");
+    });
+
+
+    interface CardsInterface {
+        color: string;
+        pictogram: string;
+        scentence: string;
+        compare: number;
     }
-];
 
+    let allCards: CardsInterface[] = [ //Muss man jedes Objekt zweimal erstellen?? Kann man das nicht mit for schleife zweimal erstellen? Wie soll das dann mit dem Satz gehen?
+        {
+            color: "#CCFFCA",
+            pictogram: "fas fa-bolt",
+            scentence: "",
+            compare: 1
+        },
+        {
+            color: "",
+            pictogram: "fas fa-wind",
+            scentence: "",
+            compare: 2
+        },
+        {
+            color: "#84FE7F",
+            pictogram: "fas fa-smog",
+            scentence: "",
+            compare: 3
+        },
+        {
+            color: "#84FE7F",
+            pictogram: "fas fa-cloud",
+            scentence: "",
+            compare: 4
+        },
+        {
+            color: "#8BCA89",
+            pictogram: "fas fa-sun",
+            scentence: "",
+            compare: 5
+        },
+        {
+            color: "#8BCA89",
+            pictogram: "fas fa-umbrella",
+            scentence: "",
+            compare: 6
+        },
+        {
+            color: "#619F5F",
+            pictogram: "fas fa-rainbow",
+            scentence: "",
+            compare: 7
+        },
+        {
+            color: "#619F5F",
+            pictogram: "fas fa-cloud-sun-rain",
+            scentence: "",
+            compare: 8
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-sun",
+            scentence: "",
+            compare: 9
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-showers-heavy",
+            scentence: "",
+            compare: 10
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-rain",
+            scentence: "",
+            compare: 11
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-moon-rain",
+            scentence: "",
+            compare: 12
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-meatball",
+            scentence: "",
+            compare: 13
+        },
+        {
+            color: "",
+            pictogram: "fas fa-temperature-low",
+            scentence: "",
+            compare: 14
+        },
+        {
+            color: "",
+            pictogram: "far fa-snowflake",
+            scentence: "",
+            compare: 15
+        },
+        {
+            color: "",
+            pictogram: "fas fa-cloud-moon",
+            scentence: "",
+            compare: 16
+        }
+    ];
 
-    
+    let cardDOMElement: HTMLElement;
+
 }
