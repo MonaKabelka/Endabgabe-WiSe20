2c76e55cf2a8b3d07c0fd4e7c52889a1490255a3
MonaKabelka <mona.kabelka.mk@gmail.com>
2021-01-31 19:59:02 +0100

    Wtfff

--- Memory/js/MemoryGame.ts
+++ Memory/js/MemoryGame.ts
@@ -15,129 +15,292 @@ namespace MemoryGame {
     btnHard.innerHTML = "Hard";
     btnWrapper.appendChild(btnHard);
 
-    // geht das mit den buttons auch kürzer?? ->function!!
+    // Eventlistener für die Buttons
     btnEasy.addEventListener("click", function (): void {
         console.log("Difficulty: Easy");
-        btnEasy.classList.add("hidden");
-        btnHard.classList.add("hidden");
-        btnMedium.classList.add("hidden");
+        hideBtns ();
     });
 
     btnMedium.addEventListener("click", function (): void {
         console.log("Difficulty: Medium");
-        btnEasy.classList.add("hidden");
-        btnHard.classList.add("hidden");
-        btnMedium.classList.add("hidden");
+        hideBtns ();
     });
 
     btnHard.addEventListener("click", function (): void {
         console.log("Difficulty: Hard");
+        hideBtns ();
+    });
+
+    // Funktion zum verstecken von den Buttons
+    function hideBtns (): void {
         btnEasy.classList.add("hidden");
         btnHard.classList.add("hidden");
         btnMedium.classList.add("hidden");
-    });
+    }
 
-
+    // Interface für Objekte von Karten _________________________________________________________________________________________________________
     interface CardsInterface {
-        color: string;
-        pictogram: string;
-        scentence: string;
+        pic: string;
         compare: number;
     }
 
-    let allCards: CardsInterface[] = [ //Muss man jedes Objekt zweimal erstellen?? Kann man das nicht mit for schleife zweimal erstellen? Wie soll das dann mit dem Satz gehen?
+    let easyCards: CardsInterface[] = [ 
+        // Kartenpaar 1
         {
-            
+            pic: "material/EasyCards/1.png",
             compare: 1
         },
         {
-            color: "",
-            pictogram: "fas fa-wind",
-            scentence: "",
+            pic: "material/EasyCards/1.png",
+            compare: 1
+        },
+        // Kartenpaar 2
+        {
+            pic: "material/EasyCards/2.png",
             compare: 2
         },
         {
-            color: "#84FE7F",
-            pictogram: "fas fa-smog",
-            scentence: "",
+            pic: "material/EasyCards/2.png",
+            compare: 2
+        },
+        // Kartenpaar 3
+        {
+            pic: "material/EasyCards/3.png",
             compare: 3
         },
         {
-            color: "#84FE7F",
-            pictogram: "fas fa-cloud",
-            scentence: "",
+            pic: "material/EasyCards/3.png",
+            compare: 3
+        },
+        // Kartenpaar 4
+        {
+            pic: "material/EasyCards/4.png",
             compare: 4
         },
         {
-            color: "#8BCA89",
-            pictogram: "fas fa-sun",
-            scentence: "",
+            pic: "material/EasyCards/4.png",
+            compare: 4
+        }
+    ];
+
+    let mediumCards: CardsInterface[] = [ 
+        // Kartenpaar 1
+        {
+            pic: "material/MediumCards/1.png",
+            compare: 1
+        },
+        {
+            pic: "material/MediumCards/2.png",
+            compare: 1
+        },
+        // Kartenpaar 2
+        {
+            pic: "material/MediumCards/3.png",
+            compare: 2
+        },
+        {
+            pic: "material/MediumCards/4.png",
+            compare: 2
+        },
+        // Kartenpaar 3
+        {
+            pic: "material/MediumCards/5.png",
+            compare: 3
+        },
+        {
+            pic: "material/MediumCards/6.png",
+            compare: 3
+        },
+        // Kartenpaar 4
+        {
+            pic: "material/MediumCards/7.png",
+            compare: 4
+        },
+        {
+            pic: "material/MediumCards/8.png",
+            compare: 4
+        },
+        // Kartenpaar 5
+        {
+            pic: "material/MediumCards/9.png",
             compare: 5
         },
         {
-            color: "#8BCA89",
-            pictogram: "fas fa-umbrella",
-            scentence: "",
+            pic: "material/MediumCards/10.png",
+            compare: 5
+        },
+        // Kartenpaar 6
+        {
+            pic: "material/MediumCards/11.png",
             compare: 6
         },
         {
-            color: "#619F5F",
-            pictogram: "fas fa-rainbow",
-            scentence: "",
+            pic: "material/MediumCards/12.png",
+            compare: 6
+        },
+        // Kartenpaar 7
+        {
+            pic: "material/MediumCards/13.png",
             compare: 7
         },
         {
-            color: "#619F5F",
-            pictogram: "fas fa-cloud-sun-rain",
-            scentence: "",
+            pic: "material/MediumCards/14.png",
+            compare: 7
+        },
+        // Kartenpaar 8
+        {
+            pic: "material/MediumCards/15.png",
             compare: 8
         },
         {
-            color: "",
-            pictogram: "fas fa-cloud-sun",
-            scentence: "",
+            pic: "material/MediumCards/16.png",
+            compare: 8
+        }
+    ];
+
+    let hardCards: CardsInterface[] = [ 
+        // Kartenpaar 1
+        {
+            pic: "material/HardCards/1.png",
+            compare: 1
+        },
+        {
+            pic: "material/HardCards/2.png",
+            compare: 1
+        },
+        // Kartenpaar 2
+        {
+            pic: "material/HardCards/3.png",
+            compare: 2
+        },
+        {
+            pic: "material/HardCards/4.png",
+            compare: 2
+        },
+        // Kartenpaar 3
+        {
+            pic: "material/HardCards/5.png",
+            compare: 3
+        },
+        {
+            pic: "material/HardCards/6.png",
+            compare: 3
+        },
+        // Kartenpaar 4
+        {
+            pic: "material/HardCards/7.png",
+            compare: 4
+        },
+        {
+            pic: "material/HardCards/8.png",
+            compare: 4
+        },
+        // Kartenpaar 5
+        {
+            pic: "material/HardCards/9.png",
+            compare: 5
+        },
+        {
+            pic: "material/HardCards/10.png",
+            compare: 5
+        },
+        // Kartenpaar 6
+        {
+            pic: "material/HardCards/11.png",
+            compare: 6
+        },
+        {
+            pic: "material/HardCards/12.png",
+            compare: 6
+        },
+        // Kartenpaar 7
+        {
+            pic: "material/HardCards/13.png",
+            compare: 7
+        },
+        {
+            pic: "material/HardCards/14.png",
+            compare: 7
+        },
+        // Kartenpaar 8
+        {
+            pic: "material/HardCards/15.png",
+            compare: 8
+        },
+        {
+            pic: "material/HardCards/16.png",
+            compare: 8
+        },
+        // Kartenpaar 9
+        {
+            pic: "material/HardCards/17.png",
             compare: 9
         },
         {
-            color: "",
-            pictogram: "fas fa-cloud-showers-heavy",
-            scentence: "",
+            pic: "material/HardCards/18.png",
+            compare: 9
+        },
+        // Kartenpaar 10
+        {
+            pic: "material/HardCards/19.png",
             compare: 10
         },
         {
-            color: "",
-            pictogram: "fas fa-cloud-rain",
-            scentence: "",
+            pic: "material/HardCards/20.png",
+            compare: 10
+        },
+        // Kartenpaar 11
+        {
+            pic: "material/HardCards/21.png",
             compare: 11
         },
         {
-            color: "",
-            pictogram: "fas fa-cloud-moon-rain",
-            scentence: "",
+            pic: "material/HardCards/22.png",
+            compare: 11
+        },
+        // Kartenpaar 12
+        {
+            pic: "material/HardCards/23.png",
             compare: 12
         },
         {
-            color: "",
-            pictogram: "fas fa-cloud-meatball",
-            scentence: "",
+            pic: "material/HardCards/24.png",
+            compare: 12
+        },
+        // Kartenpaar 13
+        {
+            pic: "material/HardCards/25.png",
             compare: 13
         },
         {
-            color: "",
-            pictogram: "fas fa-temperature-low",
-            scentence: "",
+            pic: "material/HardCards/26.png",
+            compare: 13
+        },
+        // Kartenpaar 14
+        {
+            pic: "material/HardCards/27.png",
             compare: 14
         },
         {
-            color: "",
-            pictogram: "far fa-snowflake",
-            scentence: "",
+            pic: "material/HardCards/28.png",
+            compare: 14
+        },
+        // Kartenpaar 15
+        {
+            pic: "material/HardCards/29.png",
             compare: 15
         },
         {
-            color: "",
-            pictogram: "fas fa-cloud-moon",
-            scentence: "",
+            pic: "material/HardCards/30.png",
+            compare: 15
+        },
+        // Kartenpaar 16
+        {
+            pic: "material/HardCards/31.png",
+            compare: 16
+        },
+        {
+            pic: "material/HardCards/32.png",
             compare: 16
         }
     ];
